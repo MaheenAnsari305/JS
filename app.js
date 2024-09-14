79833bbd8a3db0b382dfa18d18045192aee8f31f
Maheen Ansari <muhammadyaseen8708@gmail.com>
2024-09-14 11:19:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,269 @@
+
+// q#1
+// var userinput = prompt("Enter your city")
+// {
+
+// alert("Welcome the city of light");
+// }
+
+// q#2
+
+// var gender = prompt("Enter your gender");
+// var message = "";
+
+// if(gender === "male"){
+//     message = "Good Morning Sir";
+// }
+
+// else if (gender === "female"){
+//     message = "Good Morning Ma'am";
+// }
+
+// alert(message)
+
+// q#3
+// var color = prompt("Traffic signal");
+// var message = "";
+
+// if(color == "red"){
+//     message="Must stop";
+    
+// }
+
+
+// else if(color == "yellow"){
+//     message="Ready to move";
+
+// }
+
+// else if(color == "green"){
+//     message="Move now";
+
+// }
+// alert(message);
+
+// q#4
+
+
+// Function to check fuel level and display message if needed
+// function checkFuel() {
+    // Prompt user to enter remaining fuel in litres
+    // var  fuel = +prompt("Enter the remaining fuel in your car (in litres):");
+
+    // Check if the input is a valid number
+    // if (isNaN(fuel)) {
+    //     alert("Please enter a valid number.");
+    // } else if (fuel < 0.25) {
+        // Display a message if fuel is less than 0.25 litres
+    //     alert("Please refill the fuel in your car.");
+    // } else {
+        // Optionally, you can display a message if fuel is sufficient
+//         alert("Fuel level is sufficient.");
+//     }
+
+// }
+
+// Call the function to execute the program
+// checkFuel();
+
+
+// q#5
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("true");
+// }
+
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("true");
+// }
+
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("true");
+// }
+
+
+// var c = 12;
+// if (c++ === 13){
+// alert("true");
+// }
+
+// if (c === 13){
+//     alert("true");
+//     }
+
+
+
+//     if (++c < 14){
+//     alert(" true");
+//     }
+
+
+
+//     if(c === 14){
+//     alert(" true");
+//     }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");
+//     }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// var percentage = +prompt("Enter your percetnage");
+
+// if(percentage >=80){
+//     alert("A+")
+//     alert("Excellent");
+    
+// }
+
+// else if(percentage >=70){
+//     alert("A")
+//     alert("Good");
+
+// }
+
+// else if(percentage >=60){
+//     alert("B")
+//     alert("You need to improve");
+
+// }
+
+// else if(percentage <=60){
+//     alert("Fail")
+//     alert("Sorry");
+
+// }
+
+
+// var num = +prompt("Guess the secret number");
+
+
+// if (num === 4){
+// alert("“Bingo! Correct answer”.")
+
+// }
+
+// else(num === 1)
+//     alert("“Close enough to the correct answer”.")
+
+// q#8
+
+//    var num = prompt("enter any number") 
+//         var input =('numberInput');
+//         var number = parseInt(input, 10);
+        
+//         // Check if the input is a valid number
+//         if (isNaN(number)) {
+//             alert("result") (number)="Invalid input. Please enter a valid integer.";
+    
+//         }
+        
+//         // Check if the number is divisible by 3
+//         if (number % 3 === 0) {
+//            alert('result').innerText = `The number ${number} is divisible by 3.`;
+//         } else {
+//             alert('result').innerText = `The number ${number} is not divisible by 3.`;
+//         }
+    
+// q#9
+
+// var num = +prompt("enter any number "); 
+// var message= ""
+
+
+//     if (number % 2 === 0) {
+//   alert("return Even");
+// message= "Even"
+
+//     } else {
+//    alert= "return Odd";
+//    message="odd"
+//     }
+
+// q#10
+
+// var tem = +prompt("check the temprature");
+
+
+//  if (tem >= 40 ){
+//     message = "it's to hot outside. ";
+// }
+//  else if (tem >= 30 ){
+//     message = "The Weather today is very normal. ";
+//  }
+//  else if (tem >= 20 ){
+//     message = "Today's weather is very Cool. ";
+//  }
+//   else if  (tem >= 10 );{
+//   message = "OMG! Today's weather is so Cool . ";
+//   }
+
+
+// q#11
+
+
+// function calculator() {
+    // // Take the first number as input
+    // var a = parseFloat(prompt("Enter the first number: "));
+
+    // Take the second number as input
+    // var b = parseFloat(prompt("Enter the second number: "));
+
+    // Take the operation as input
+    // var operation = prompt("Enter the operation (+, -, *, /, %): ");
+
+    // Use if statements to perform the operation
+    // var result;
+    // if (operation === "+") {
+    //     result = a + b;
+    // } else if (operation === "-") {
+    //     result = a - b;
+    // } else if (operation === "*") {
+    //     result = a * b;
+    // } else if (operation === "/") {
+    //     if (b !== 0) {
+    //         result = a / b;
+    //     } else {
+    //         alert("Error! Division by zero is not allowed.");
+    //         return;
+    //     }
+    // } else if (operation === "%") {
+    //     result = a % b;
+    // } else {
+    //     alert("Error! Invalid operation.");
+    //     return;
+    // }
+
+    // Print the result
+    // alert("The result is: " + result);
+// }
+
+// Call the function
+// calculator();
+
+
+
+
+
+
+
+
+
